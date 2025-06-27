7156c6f7fa22ab26de292557bd3990b5cf4a696d
viniciuscgodoy <162365226+viniciuscgodoy@users.noreply.github.com>
2025-06-27 08:32:10 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/analisadorv1/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/analisadorv4/' : '/',
   build: {
     outDir: 'dist'
   },
